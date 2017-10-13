f14c95cb6ffb10dacb409c2700970a0b5bae3c86
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-13 17:58:07 +0200

    Try usual setup of CMSSW

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 ls
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 printenv
-ls /cvmfs/cms.cern.ch/common/scramv1
-/cvmfs/cms.cern.ch/common/scramv1 project CMSSW CMSSW_8_0_28
-# which scram
-# which cmsrel
-# cmsrel CMSSW_8_0_28
+which scram
+which cmsrel
+cmsrel CMSSW_8_0_28
+# ls /cvmfs/cms.cern.ch/common/scramv1
+# /cvmfs/cms.cern.ch/common/scramv1 project CMSSW CMSSW_8_0_28
