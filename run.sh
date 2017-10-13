8b7ef1bd4e20841ebcc074a4c032c09b9d18d551
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-13 18:00:36 +0200

    Try some more CMSSW commands

--- run.sh
+++ run.sh
@@ -5,6 +5,10 @@ source /cvmfs/cms.cern.ch/cmsset_default.sh
 printenv
 which scram
 which cmsrel
-cmsrel CMSSW_8_0_28
+# cmsrel CMSSW_8_0_28
+cmsrel CMSSW_9_2_13
+cd CMSSW_9_2_13/src
+cmsenv
+git cms-addpkg FWCore/Framework
 # ls /cvmfs/cms.cern.ch/common/scramv1
 # /cvmfs/cms.cern.ch/common/scramv1 project CMSSW CMSSW_8_0_28
