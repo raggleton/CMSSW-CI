1405605c977a416aa503c309a271e46287d9d465
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-13 17:51:05 +0200

    Try cmsrel alias

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 ls
 printenv
-which scram
-which cmsrel
-cmsrel CMSSW_8_0_28
+ls /cvmfs/cms.cern.ch/common/scramv1
+/cvmfs/cms.cern.ch/common/scramv1 project CMSSW CMSSW_8_0_28
+# which scram
+# which cmsrel
+# cmsrel CMSSW_8_0_28
