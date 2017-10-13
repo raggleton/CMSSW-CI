2ff72e1ebac4ea0ab8542a3e0f8cdded4b051ccf
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-13 18:05:42 +0200

    More cms commands

--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 ls
+cat /cvmfs/cms.cern.ch/cmsset_default.sh
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 printenv
 which scram
-which cmsrel
+# which cmsrel
 # cmsrel CMSSW_8_0_28
-cmsrel CMSSW_9_2_13
-cd CMSSW_9_2_13/src
+# cmsrel CMSSW_9_2_13
+# cd CMSSW_9_2_13/src
+# cmsenv
+# git cms-addpkg FWCore/Framework
+# ls /cvmfs/cms.cern.ch/common/scramv1
+/cvmfs/cms.cern.ch/common/scramv1 project CMSSW CMSSW_8_0_28
+cd CMSSW_8_0_28/src
 cmsenv
 git cms-addpkg FWCore/Framework
-# ls /cvmfs/cms.cern.ch/common/scramv1
-# /cvmfs/cms.cern.ch/common/scramv1 project CMSSW CMSSW_8_0_28
+scram build
