93c5b0ddca5b42b1aa2e19ac372fea41f6d78b4a
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-14 23:44:55 +0200

    Move installs to Dockerfile for speed in future

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,14 @@ FROM cern/slc6-base
 
 USER root
 
+RUN yum install -y svn git
+
+RUN yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
+
+RUN yum install -y cvmfs cvmfs-config-default
+
+RUN mkdir -p /etc/cvmfs
+
 WORKDIR /app
 
 ADD . /app
--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
-yum install -y cvmfs cvmfs-config-default
-yum install -y svn git
-mkdir -p /etc/cvmfs
+shopt -s expand_aliases
+
+# SETUP CVMFS FOR CMS
+# -------------------
+# yum install -y svn git
+# yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
+# yum install -y cvmfs cvmfs-config-default
+# mkdir -p /etc/cvmfs
 cvmfs_config setup
 cp default.local /etc/cvmfs/default.local
 cvmfs_config setup
@@ -16,14 +20,10 @@ export CMSSW_GIT_REFERENCE=/app/cmssw.git
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 echo $SCRAM_ARCH
 alias cmsrel
-
-# cmsrel CMSSW_9_2_13
-# cd CMSSW_9_2_13/src
-# cmsenv
-
-cmsrel CMSSW_8_0_28
-cd CMSSW_8_0_28/src
-cmsenv
+export SCRAM_ARCH="slc6_amd64_gcc530"
+eval `cmsrel CMSSW_8_0_24_patch1`
+cd CMSSW_8_0_24_patch1/src
+eval `scramv1 runtime -sh`
 git config --global user.name "Joe Bloggs"
 git config --global user.email "a@b.c"
 git config --global user.github "testUHH"
