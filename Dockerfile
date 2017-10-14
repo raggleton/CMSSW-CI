9d1755af3b1192bde41167ea71fd9f63d44b65a6
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-14 22:35:44 +0200

    Move CVMFS setup to running container rather than build

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,6 @@
 FROM cern/slc6-base
-WORKDIR /code
-ADD . /code
 USER root
-RUN yum install https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
-RUN yum install cvmfs cvmfs-config-default
-RUN cvmfs_config setup
-RUN mkdir /etc/cvmfs/
-RUN cp /code/default.local /etc/cvmfs/default.local
-RUN cvmfs_config probe
-
-CMD ["cat", "/cvmfs/cms.cern.ch/cmsset_default.sh"]
+RUN mkdir -p /etc/cvmfs/
+WORKDIR /app
+ADD . /app
+CMD ["source", "run.sh"]
--- run.sh
+++ run.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-ls
-uname
-cat /cvmfs/cms.cern.ch/cmsset_default.sh
-# source /cvmfs/cms.cern.ch/cmsset_default.sh
-# printenv
-# which scram
-# which cmsrel
-# cmsrel CMSSW_8_0_28
+yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
+yum install -y cvmfs cvmfs-config-default
+cvmfs_config setup
+cp default.local /etc/cvmfs/default.local
+cvmfs_config setup
+mkdir -p /cvmfs/cms.cern.ch
+mount -t cvmfs cms.cern.ch /cvmfs/cms.cern.ch
+ls /cvmfs/cms.cern.ch/
+
+export CMSSW_GIT_REFERENCE=/app/cmssw.git
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
 # cmsrel CMSSW_9_2_13
 # cd CMSSW_9_2_13/src
 # cmsenv
-# git cms-addpkg FWCore/Framework
-# ls /cvmfs/cms.cern.ch/common/scramv1
-# /cvmfs/cms.cern.ch/common/scramv1 project CMSSW CMSSW_8_0_28
-# cd CMSSW_8_0_28/src
-# cmsenv
-# git cms-addpkg FWCore/Framework
-# scram build
+
+cmsrel CMSSW_8_0_28
+cd CMSSW_8_0_28/src
+cmsenv
+git config --global user.name "Joe Bloggs"
+git config --global user.email "a@b.c"
+git config --global user.github "jjjjjjj"
+
+git cms-addpkg -y --https FWCore/Framework
+scram build
