ba6712494c82f7ca33ebabf34532f06592263466
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-14 18:06:26 +0200

    Run SLC6 docker image

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 ls
 cat /cvmfs/cms.cern.ch/cmsset_default.sh
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-printenv
-which scram
+# source /cvmfs/cms.cern.ch/cmsset_default.sh
+# printenv
+# which scram
 # which cmsrel
 # cmsrel CMSSW_8_0_28
 # cmsrel CMSSW_9_2_13
@@ -12,8 +12,8 @@ which scram
 # cmsenv
 # git cms-addpkg FWCore/Framework
 # ls /cvmfs/cms.cern.ch/common/scramv1
-/cvmfs/cms.cern.ch/common/scramv1 project CMSSW CMSSW_8_0_28
-cd CMSSW_8_0_28/src
-cmsenv
-git cms-addpkg FWCore/Framework
-scram build
+# /cvmfs/cms.cern.ch/common/scramv1 project CMSSW CMSSW_8_0_28
+# cd CMSSW_8_0_28/src
+# cmsenv
+# git cms-addpkg FWCore/Framework
+# scram build
