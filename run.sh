72b1af658a0c0fc525562e99996e8bc0c62f70d7
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-17 14:13:02 +0200

    Increase cores when building and remove old comments

--- run.sh
+++ run.sh
@@ -3,17 +3,6 @@
 # CRUCIAL for cmsrel, etc as aliases not expanded in non-interactive shells
 shopt -s expand_aliases
 
-# SETUP CVMFS FOR CMS
-# -------------------
-# yum update -y && yum install -y svn git glib gcc
-# yum update -y && yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
-# yum update -y && yum install -y cvmfs cvmfs-config-default
-# mkdir -p /etc/cvmfs
-# cvmfs_config setup
-# cp default.local /etc/cvmfs/default.local
-# cvmfs_config setup
-# mkdir -p /cvmfs/cms.cern.ch
-# mount -t cvmfs cms.cern.ch /cvmfs/cms.cern.ch
 ls /cvmfs/cms.cern.ch/
 
 export CMSSW_GIT_REFERENCE=/app/cmssw.git
@@ -25,9 +14,11 @@ export SCRAM_ARCH="slc6_amd64_gcc530"
 eval `cmsrel CMSSW_8_0_24_patch1`
 cd CMSSW_8_0_24_patch1/src
 eval `scramv1 runtime -sh`
+
+# You can use dummy values here if not pushing, but is required for pulling
 git config --global user.name "Joe Bloggs"
 git config --global user.email "a@b.c"
 git config --global user.github "testUHH"
 
 git cms-addpkg -y --https RecoBTag
-scram build
+scram build -j9
