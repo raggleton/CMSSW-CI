57b90e9b9f43cd2e29e114958058edbe8c0d1e53
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-25 16:07:25 +0200

    Move from wget to curl as curl included via CVMFS, wget isnt

--- setup_fastjet.sh
+++ setup_fastjet.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Setup fastjet & fastjet-contrib
+# NB use curl not wget as curl available by cvmfs, wget isnt
 FJVER="3.2.2"
 FJINSTALLDIR="fastjet-install"
-wget http://fastjet.fr/repo/fastjet-${FJVER}.tar.gz
+curl -O http://fastjet.fr/repo/fastjet-${FJVER}.tar.gz
 tar xvzf fastjet-${FJVER}.tar.gz
 mkdir ${FJINSTALLDIR}
 cd fastjet-${FJVER}    
@@ -12,12 +13,13 @@ make check
 make install
 
 cd ..
-g++ short-example.cc -o short-example `${FJINSTALLDIR}/bin/fastjet-config --cxxflags --libs —plugins`
+ls
+g++ short-example.cc -o short-example `${FJINSTALLDIR}/bin/fastjet-config --cxxflags --libs -plugins`
 ./short-example
 
 
 FJCONTRIBVER="1.030"
-wget http://fastjet.hepforge.org/contrib/downloads/fjcontrib-${FJCONTRIBVER}.tar.gz
+curl -O http://fastjet.hepforge.org/contrib/downloads/fjcontrib-${FJCONTRIBVER}.tar.gz
 tar xvzf fjcontrib-${FJCONTRIBVER}.tar.gz
 cd fjcontrib-${FJCONTRIBVER}
 ./configure --fastjet-config=`pwd`/../${FJINSTALLDIR}/bin/fastjet-config
