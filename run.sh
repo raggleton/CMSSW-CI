d351e721d58b8d47c3568f3eda64240eaeea682d
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-27 16:15:29 +0200

    Makedirs first

--- run.sh
+++ run.sh
@@ -9,11 +9,15 @@ shopt -s expand_aliases
 # Check CVMSFS setup correctly
 # Need to manually setup STIECONF as we're not a proper site
 ls /cvmfs/cms.cern.ch/
+mkdir -p /etc/cvmfs/config.d/
 cp cms.cern.ch.local /etc/cvmfs/config.d/cms.cern.ch.local
+mkdir -p /etc/cvmfs/
 cp default.local /etc/cvmfs/default.local
 service autofs restart
 cvmfs_config setup
 cvmfs_config chksetup
+# test this actually links
+ls -l /cvmfs/cms.cern.ch/SITECONF/local/
 
 
 # Store top location
