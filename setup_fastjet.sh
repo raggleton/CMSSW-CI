1041b10d96a7a5eb3aef25abfd47264b9c5d8b10
Robin Aggleton <robin.aggleton@cern.ch>
2017-10-31 14:56:10 +0100

    Make the fastjet-install dir var more sensible

--- setup_fastjet.sh
+++ setup_fastjet.sh
@@ -6,19 +6,19 @@ MAKEFLAGS="-j $(grep -c ^processor /proc/cpuinfo)"
 # Setup fastjet & fastjet-contrib
 # NB use curl not wget as curl available by cvmfs, wget isnt
 export FJVER="3.2.1"
-FJINSTALLDIR="fastjet-install"
+FJINSTALLDIR="$(pwd)/fastjet-install"
 curl -O http://fastjet.fr/repo/fastjet-${FJVER}.tar.gz
 tar xzf fastjet-${FJVER}.tar.gz
-mkdir ${FJINSTALLDIR}
+mkdir "${FJINSTALLDIR}"
 cd fastjet-${FJVER}    
-./configure --prefix=$(pwd)/../${FJINSTALLDIR}/ --enable-allplugins --enable-allcxxplugins CXXFLAGS=-fPIC
+./configure --prefix="${FJINSTALLDIR}" --enable-allplugins --enable-allcxxplugins CXXFLAGS=-fPIC
 make "$MAKEFLAGS"
 make check
 make install
 
 cd ..
 # Add fastjet-config to PATH
-export PATH=$(pwd)/${FJINSTALLDIR}/bin:$PATH
+export PATH="${FJINSTALLDIR}/bin":$PATH
 
 # Test fastjet
 ls
@@ -33,7 +33,7 @@ cd fjcontrib-${FJCONTRIBVER}
 # add HOTVR from SVN - do it this way until it becomes a proper contrib
 svn co http://fastjet.hepforge.org/svn/contrib/contribs/HOTVR/trunk HOTVR/
 # although we add fastjet-config to path, due to a bug explicitly stating this ensure the necessary fragile library gets built 
-./configure --fastjet-config=$(pwd)/../${FJINSTALLDIR}/bin/fastjet-config CXXFLAGS=-fPIC
+./configure --fastjet-config="${FJINSTALLDIR}/bin/fastjet-config" CXXFLAGS=-fPIC
 make "$MAKEFLAGS"
 make check
 make install
