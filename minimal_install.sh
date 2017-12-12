2a3417b91259cb55ac31be84b9d61dc689535f52
Robin Aggleton <robin.aggleton@cern.ch>
2017-12-12 15:00:04 +0100

    Auto answer y

--- minimal_install.sh
+++ minimal_install.sh
@@ -11,7 +11,7 @@ export SCRAM_ARCH=slc6_amd64_gcc630
 eval `cmsrel CMSSW_9_4_1`
 cd CMSSW_9_4_1/src
 eval `scramv1 runtime -sh`
-git cms-init
+git cms-init -y
 scram b -j 20
 cd $CMSSW_BASE/src
 git clone -b RunII_94X_v1 https://github.com/UHH2/UHH2.git
