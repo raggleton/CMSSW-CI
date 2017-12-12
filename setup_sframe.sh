f330e8701912c626050cf053243508cc2e5de07c
Robin Aggleton <robin.aggleton@cern.ch>
2017-12-12 16:15:08 +0100

    Typo

--- setup_sframe.sh
+++ setup_sframe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # Done this way as builtin git is old and doesn't support git clone -b <TAG> <REPO>
-# The --depth 1 stops you form checking everything out immediately
+# The --depth 1 stops you from checking everything out immediately
 git clone --depth 1 https://github.com/UHH2/SFrame.git
 cd SFrame
 git checkout tags/SFrame-04-00-01
