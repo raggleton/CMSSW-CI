df57c7c355fb6e60b01429f6ed391fc582a2f74d
Robin Aggleton <robin.aggleton@cern.ch>
2018-01-08 13:32:31 +0100

    Try with variable

--- run.sh
+++ run.sh
@@ -10,88 +10,75 @@ set -o pipefail # exit status of the last command that threw a non-zero exit cod
 shopt -s expand_aliases
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-ls /etc/
-mkdir -p /etc/cvmfs/config.d/
-mkdir -p /etc/cms/
 
 
+# ls /etc/
+# mkdir -p /etc/cvmfs/config.d/
+# mkdir -p /etc/cms/
+# # Avoid xroot client warnings
+# touch /etc/profile.d/xrootd-protocol.sh
+# echo '
+# export XrdSecPROTOCOL=unix
+# ' > /etc/profile.d/xrootd-protocol.sh
+# # Manual SITECONF, ideally at some point part of /cvmfs/cms.cern.ch
+# mkdir -p /etc/cms/SITECONF/T2_UK_London_IC/{JobConfig,PhEDEx}
+# ln -s /etc/cms/SITECONF/T2_UK_London_IC /etc/cms/SITECONF/local
+# echo '
+# <storage-mapping>
+#   <lfn-to-pfn protocol="root" destination-match=".*"
+#     path-match="(.*)" result="root://eospublic.cern.ch/$1"/>
+#   <lfn-to-pfn protocol="xrootd" destination-match=".*" chain="root"
+#     path-match="(.*)" result="$1"/>
 
-# Avoid xroot client warnings
-touch /etc/profile.d/xrootd-protocol.sh
-echo '
-export XrdSecPROTOCOL=unix
-' > /etc/profile.d/xrootd-protocol.sh
+#   <!-- fallback xrootd rules -->
+#   <lfn-to-pfn protocol="xrootd-uk2" destination-match=".*"
+#     path-match="(.*)" result="root://xrootd-uk-cms.gridpp.rl.ac.uk/$1"/>
+#   <lfn-to-pfn protocol="xrootd-eu" destination-match=".*"
+#     path-match="(.*)" result="root://xrootd.ba.infn.it/$1"/>
+#   <!-- combine all fallbacks into 1 rule, cmssw can only have 1 fallback -->
+#   <!-- server chain stops when one server responds - even if it does not have
+# the file -->
+#   <lfn-to-pfn protocol="fallbacks" destination-match=".*"
+#    path-match="(.*)"
+# result="root://cms-xrd-global.cern.ch,xrootd.ba.infn.it,xrootd.unl.edu/$1?tried=gfe02.grid.hep.ph.ic.ac.uk"/>
 
+#   <pfn-to-lfn protocol="direct" destination-match=".*"
+#     path-match=".*(/store/.*)" result="$1"/>
+#   <pfn-to-lfn protocol="root" destination-match=".*" chain="direct"
+#     path-match="(.*)" result="$1"/>
+#   <pfn-to-lfn protocol="xrootd" destination-match=".*" chain="root"
+#     path-match="(.*)" result="$1"/>
+# </storage-mapping>
+# ' > /etc/cms/SITECONF/T2_UK_London_IC/PhEDEx/storage.xml
+# echo '
+# <site-local-config>
+#         <site name="T2_UK_London_IC">
+#         <event-data>
+#                 <catalog url="trivialcatalog_file:/etc/cms/SITECONF/local/PhEDEx/storage.xml?protocol=root"/>
+#                 <catalog url="trivialcatalog_file:/etc/cms/SITECONF/local/PhEDEx/storage.xml?protocol=fallbacks"/>
+#         </event-data>
+#                 <calib-data>
+#                         <frontier-connect>
+#                                 <proxy url="http://squid02.gridpp.rl.ac.uk:3128"/>
+#                                 <proxy url="http://squid03.gridpp.rl.ac.uk:3128"/>
+#                                 <proxy url="http://squid04.gridpp.rl.ac.uk:3128"/>
+#                                 <proxy url="http://squid05.gridpp.rl.ac.uk:3128"/>
+#                                 <server url="http://cmsfrontier.cern.ch:8000/FrontierInt"/>
+#                                 <server url="http://cmsfrontier1.cern.ch:8000/FrontierInt"/>
+#                                 <server url="http://cmsfrontier2.cern.ch:8000/FrontierInt"/>
+#                                <server url="http://cmsfrontier3.cern.ch:8000/FrontierInt"/>
+#                         </frontier-connect>
+#         </calib-data>
+#         </site>
+# </site-local-config>
+# ' > /etc/cms/SITECONF/T2_UK_London_IC/JobConfig/site-local-config.xml
 
-# Manual SITECONF, ideally at some point part of /cvmfs/cms.cern.ch
-mkdir -p /etc/cms/SITECONF/T2_UK_London_IC/{JobConfig,PhEDEx}
-ln -s /etc/cms/SITECONF/T2_UK_London_IC /etc/cms/SITECONF/local
-echo '
-<storage-mapping>
-  <lfn-to-pfn protocol="root" destination-match=".*"
-    path-match="(.*)" result="root://eospublic.cern.ch/$1"/>
-  <lfn-to-pfn protocol="xrootd" destination-match=".*" chain="root"
-    path-match="(.*)" result="$1"/>
-
-  <!-- fallback xrootd rules -->
-  <lfn-to-pfn protocol="xrootd-uk2" destination-match=".*"
-    path-match="(.*)" result="root://xrootd-uk-cms.gridpp.rl.ac.uk/$1"/>
-  <lfn-to-pfn protocol="xrootd-eu" destination-match=".*"
-    path-match="(.*)" result="root://xrootd.ba.infn.it/$1"/>
-  <!-- combine all fallbacks into 1 rule, cmssw can only have 1 fallback -->
-  <!-- server chain stops when one server responds - even if it does not have
-the file -->
-  <lfn-to-pfn protocol="fallbacks" destination-match=".*"
-   path-match="(.*)"
-result="root://cms-xrd-global.cern.ch,xrootd.ba.infn.it,xrootd.unl.edu/$1?tried=gfe02.grid.hep.ph.ic.ac.uk"/>
-
-  <pfn-to-lfn protocol="direct" destination-match=".*"
-    path-match=".*(/store/.*)" result="$1"/>
-  <pfn-to-lfn protocol="root" destination-match=".*" chain="direct"
-    path-match="(.*)" result="$1"/>
-  <pfn-to-lfn protocol="xrootd" destination-match=".*" chain="root"
-    path-match="(.*)" result="$1"/>
-</storage-mapping>
-' > /etc/cms/SITECONF/T2_UK_London_IC/PhEDEx/storage.xml
-echo '
-<site-local-config>
-        <site name="T2_UK_London_IC">
-        <event-data>
-                <catalog
-url="trivialcatalog_file:/etc/cms/SITECONF/local/PhEDEx/storage.xml?protocol=root"/>
-                <catalog
-url="trivialcatalog_file:/etc/cms/SITECONF/local/PhEDEx/storage.xml?protocol=fallbacks"/>
-        </event-data>
-                <calib-data>
-                        <frontier-connect>
-                                <proxy
-url="http://squid02.gridpp.rl.ac.uk:3128"/>
-                                <proxy
-url="http://squid03.gridpp.rl.ac.uk:3128"/>
-                                <proxy
-url="http://squid04.gridpp.rl.ac.uk:3128"/>
-                                <proxy
-url="http://squid05.gridpp.rl.ac.uk:3128"/>
-                                <server
-url="http://cmsfrontier.cern.ch:8000/FrontierInt"/>
-                                <server
-url="http://cmsfrontier1.cern.ch:8000/FrontierInt"/>
-                                <server
-url="http://cmsfrontier2.cern.ch:8000/FrontierInt"/>
-                               <server
-url="http://cmsfrontier3.cern.ch:8000/FrontierInt"/>
-                        </frontier-connect>
-        </calib-data>
-        </site>
-</site-local-config>
-' > /etc/cms/SITECONF/T2_UK_London_IC/JobConfig/site-local-config.xml
-
-# Hack for CMSSW bug (?)
-ln -s /cvmfs/cms.cern.ch/SITECONF /etc/cms/SITECONF
+# # Hack for CMSSW bug (?)
+# ln -s /cvmfs/cms.cern.ch/SITECONF /etc/cms/SITECONF
 
 # Check CVMSFS
 ls /cvmfs/cms.cern.ch/
-ls -l /cvmfs/cms.cern.ch/SITECONF/local/
+ls -l /cvmfs/cms.cern.ch/SITECONF/
 
 # Store top location
 WORKDIR=$(pwd)
